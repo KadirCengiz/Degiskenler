123b08fad342e9af06a48fb9423dfba2dd35753c
KadirCengiz <115431038+KadirCengiz@users.noreply.github.com>
2022-12-01 12:09:45 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int uzunKenar, kısaKenar;
+        kısaKenar=10;
+        uzunKenar=20;
+
+        //int alan = kısaKenar*uzunKenar;
+        //int cevre = 2*(kısaKenar+uzunKenar);
+
+        int alan = kısaKenar*uzunKenar;
+        int cevre = 2*(kısaKenar+uzunKenar);
+        
+        System.out.println(alan);
+        System.out.println(cevre);
+    }
+}
